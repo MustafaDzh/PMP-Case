13031dbf19ec1dca9fba9add6855b3bda181cda5
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-08-09 16:02:58 +0300

    in progress

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 95,
+      centeredSlides: true,
+      direction: 'horizontal',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      }
+    });
+  });
