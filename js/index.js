e51664301b31e324c1ff5a3777f21d4d4266897c
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-08-12 16:30:34 +0300

    final.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.swiper', {
+  var swiper = new Swiper('.swiper', {
       loop: true,
       slidesPerView: 1,
       spaceBetween: 95,
       centeredSlides: true,
       direction: 'horizontal',
-      // autoplay: {
-      //   delay: 3000,
-      //   disableOnInteraction: false,
-      // }
-    });
+      navigation: {
+        nextEl: '.right-button',
+        prevEl: '.left-button'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+      dynamicBullets: true,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
   });
+});
